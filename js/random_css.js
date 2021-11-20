5f8292dc67e012252d52a659c25597ab307a2f41
matth <matth@polio.be>
2021-11-20 10:02:42 +0100

    fp

--- js/random_css.js
+++
@@ -1,7 +0,0 @@
-var css = ['blue.css', 'brown.css', 'default.css', 'green.css', 'red.css', 'yellow.css'];
-
-$(document).ready(function() {
-    var style = css[Math.floor(Math.random() * css.length)];
-
-    $('head').append('<link rel="stylesheet" href="css/' + style + '" type="text/css" />');
-});
